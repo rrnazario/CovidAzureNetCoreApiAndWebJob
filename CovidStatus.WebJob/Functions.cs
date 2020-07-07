6acc33adedf4aa4ba3c9efe1400cc8fad0464640
Rogerson Reis Nazario <M6024365@mgmt.tlrg.com>
2020-07-07 08:45:53 -0300

    Removing useless comments

--- CovidStatus.WebJob/Functions.cs
+++ CovidStatus.WebJob/Functions.cs
@@ -15,7 +15,6 @@ namespace CovidStatus.WebJob
         [NoAutomaticTrigger]
         public static void CheckCovidIndexes(TextWriter log)
         {
-            //log.WriteLine(message);
             var baseEndpoint = EnvironmentHelper.GetInfoFromConfig("APIBaseAddress");
 
             var covidInformation = HttpClientHelper.GetContentFromRequest<Covid>($"{baseEndpoint}/Health/GetCovidResults");
